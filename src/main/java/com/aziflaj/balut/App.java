4c7c1850de72818b851a97c738cc520aaac0e5ef
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-18 11:35:30 +0100

    added dice folder structure and images

--- src/main/java/com/aziflaj/balut/App.java
+++ src/main/java/com/aziflaj/balut/App.java
@@ -1,21 +1,14 @@
 package com.aziflaj.balut;
 
 import javax.swing.*;
-import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                MainView app;
-                try {
-                    app = new MainView();
-                    app.setVisible(true);
+                //TODO: Start the application
 
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
             }
         });
     }
