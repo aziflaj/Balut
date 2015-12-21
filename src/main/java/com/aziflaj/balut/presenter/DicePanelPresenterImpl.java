fe12e7746e413905d56e792b0538b6f8a19b62f8
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-21 13:21:51 +0100

    fixed the rendering issue (dices show their value) and named member variables as they should

--- src/main/java/com/aziflaj/balut/presenter/DicePanelPresenterImpl.java
+++ src/main/java/com/aziflaj/balut/presenter/DicePanelPresenterImpl.java
@@ -3,46 +3,46 @@ package com.aziflaj.balut.presenter;
 import com.aziflaj.balut.model.Dice;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * The implementation of {@code DicePanelPresenter}, holding the functionality of the Presenter
  */
 public class DicePanelPresenterImpl implements DicePanelPresenter {
-    ArrayList<Dice> diceList;
+    ArrayList<Dice> mDiceList;
 
     /**
      * Create the list of the dice used by the game (5 dice)
      */
     public DicePanelPresenterImpl() {
-        diceList = new ArrayList<>();
+        mDiceList = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
             Dice dice = new Dice();
-            diceList.add(dice);
+            mDiceList.add(dice);
         }
     }
 
     @Override
     public ArrayList<Dice> getDiceList() {
-        return diceList;
+        return mDiceList;
     }
 
     @Override
     public void rollDices(int[] indices) {
-        Random rnd = new Random();
-        // TODO: Implement
+        for (int i : indices) {
+            mDiceList.get(i).roll();
+        }
     }
 
     @Override
     public void rollAllDices() {
-        for (Dice dice : diceList) {
+        for (Dice dice : mDiceList) {
             dice.roll();
         }
     }
 
     @Override
     public Dice getDiceAt(int i) {
-        return diceList.get(i);
+        return mDiceList.get(i);
     }
 }
--- src/main/java/com/aziflaj/balut/view/DicePanel.java
+++ src/main/java/com/aziflaj/balut/view/DicePanel.java
@@ -7,34 +7,32 @@ import com.aziflaj.balut.presenter.DicePanelPresenterImpl;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DicePanel extends JPanel {
     DicePanelPresenter mPresenter;
-    JButton rollDicesBtn;
-    ArrayList<DiceView> diceViewArrayList;
+    JButton mRollDicesBtn;
+    ArrayList<DiceView> mDiceViewList;
 
     public DicePanel() {
         mPresenter = new DicePanelPresenterImpl();
 
-        rollDicesBtn = new JButton("Roll the dice");
-        diceViewArrayList = new ArrayList<>();
+        mRollDicesBtn = new JButton("Roll the dice");
+        mDiceViewList = new ArrayList<>();
 
         for (Dice d : mPresenter.getDiceList()) {
             DiceView diceView = new DiceView(d);
-            diceViewArrayList.add(diceView);
+            mDiceViewList.add(diceView);
         }
 
         // set the layout as vertical
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(rollDicesBtn);
-        for (DiceView dv : diceViewArrayList) {
+        this.add(mRollDicesBtn);
+        for (DiceView dv : mDiceViewList) {
             this.add(dv);
         }
 
-        rollDicesBtn.addActionListener(new ActionListener() {
+        mRollDicesBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 rollDices();
@@ -45,8 +43,8 @@ public class DicePanel extends JPanel {
     private void rollDices() {
         mPresenter.rollAllDices();
         int i = 0;
-        for (DiceView dv : diceViewArrayList) {
-            dv.updateDice(mPresenter.getDiceAt(i));
+        for (DiceView dv : mDiceViewList) {
+            dv.updateDice(mPresenter.getDiceAt(i++));
         }
     }
 }
